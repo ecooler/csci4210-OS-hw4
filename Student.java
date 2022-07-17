bd1b68834f4c5f3ae0ff521de527ad43549a1242
powcoder代写 <powcoder@163.com>
2022-07-17 10:32:33 +0800

    程序代写 加微信 powcoder Programming Help Add Wechat powcoder

---
+++ Student.java
@@ -0,0 +1,30 @@
+public class Student   implements java.io.Serializable
+{
+  private String name;
+  private String id;
+  private double gpa;
+
+  public Student( String name, String id, double gpa )
+  {
+    this.name = name;
+    this.id = id;
+    this.gpa = gpa;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public double getGpa() {
+    return gpa;
+  }
+
+  public void setGpa( double gpa ) {
+    this.gpa = gpa;
+  }
+}
+
